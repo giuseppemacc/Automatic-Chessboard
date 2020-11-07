9a97c1243c5cbe58ba4941ac25fb309237c806b4
giuseppe <giuseppemacc13@gmail.com>
2020-11-07 12:33:56 +0100

    isolamento funzioni di imageProcessing

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,49 +2,47 @@ import numpy as np
 from PIL import Image
 import os
 
-def binarize_by2filter(image, filter, offset, with_bin_image=False):
+def _binarize_by2filter(np_image, filter, offset, with_bin_image=False):
     """
     riceve un immagine e ritorna un array di pixel binarizzati in base a due filtri
     """
-    np_image = np.array(image)
     filter = np.array(filter)
     # False -> 0 -> nero
     # True -> 255 -> bianco
     np_bin =    np.logical_or(np.logical_and((filter[0] - offset[0]) < np_image, np_image < (filter[0] + offset[0])), \
                 np.logical_and((filter[1] - offset[1]) < np_image, np_image < (filter[1] + offset[1])))
 
-    for i in range(image.height):
-        for j in range(image.width):
+    # questo ciclo rende l'rgb dei pixel tutti False nel caso che non fossero tutti True
+    for i in range(np_bin.shape[0]):
+        for j in range(np_bin.shape[1]):
             if np.array_equal(np_bin[i][j], [True, True, True]):
                 pass
             else:
                 np_bin[i][j] = [False, False, False]
 
-    bool_np_checkboard = _bool_checkboard(np_bin)
-
     if with_bin_image:
         return {"image": Image.fromarray(np.uint8(np_bin * 255)),
-                "bool": bool_np_checkboard}
+                "np": np_bin}
     else:
-        return bool_np_checkboard
+        return np_bin
 
-def _bool_checkboard(bool_np):
+def _bool_checkboard(bool_np,dim):
     """
     riceve bool_np della scacchiera con ogni singolo pixel binarizzato
     e ritorna bool_np_checkboard come se fosse una scacchiera quindi
     divide bool_np ricevuto come parametro in un 8x8 e se in una casella è presente anche un solo pixel True la casella 
     corrispondente di bool_np_checkboard diventa True
     """
-    # TODO permettere di fare la stessa cosa ma con le panchine
-    size = int(bool_np.shape[0])
-    bool_np_checkboard = np.full((8,8),None)         
 
-    step = int(size/8)
+    size = [int(bool_np.shape[0]), int(bool_np.shape[1])]
+    bool_np_checkboard = np.full(dim, None)         
+
+    step = int(size[0]/8) # considerando come shape[0] l'altezza
     count_x = 0
     count_y = 0
-    for i in range(0, size, step):
+    for i in range(0, size[0], step):
         y = [i, i+step]
-        for j in range(0, size, step):
+        for j in range(0, size[1], step):
             x = [j, j+step]
 
             cube = np.array( [i[x[0]:x[1]] for i in bool_np[y[0]:y[1]]] )
@@ -64,8 +62,13 @@ def _bool_checkboard(bool_np):
 if __name__ == '__main__':
     
     # create image
-    im = Image.open('image/2_ideal.jpeg').resize((120, 120))
-    im.show()
+    im_pn = Image.open('image/2_ideal_pn.jpeg').resize((30, 120))
+    im_pn.show()
+    im_np_pn = np.array(im_pn)
+
+    im_grid = Image.open('image/2_ideal.jpeg').resize((120, 120))
+    im_grid.show()
+    im_np_grid = np.array(im_grid)
 
     # white and black filter color
     white = [239,228,176]
@@ -73,7 +76,11 @@ if __name__ == '__main__':
 
     # dic_bin è un dizionario contenente l'image binarizzata (bianco e nero) e un array booleano
     # posso anche scegliere di ritornare solo la schacchiera in bool mettendo with_bin_image=False (che lo è già di default) 
-    dic_bin = binarize_by2filter(im, [white,black], [10, 10], with_bin_image=True)
-    
-    print(dic_bin["bool"])
-    dic_bin["image"].show()
+    dic_bin_pn = _binarize_by2filter(im_np_pn, [white,black], [10, 10], with_bin_image=True)
+    dic_bin_pn["image"].show()
+
+    dic_bin_grid = _binarize_by2filter(im_np_grid, [white,black], [10, 10], with_bin_image=True)
+    dic_bin_grid["image"].show()
+
+    print(_bool_checkboard(dic_bin_pn["np"],(8,2)))
+    print(_bool_checkboard(dic_bin_grid["np"],(8,8)))

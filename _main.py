68d75d2dd45813d57d9ad1890126c164dc0422bd
giuseppe <giuseppemacc13@gmail.com>
2020-11-11 17:26:43 +0100

    Create _main.py

---
+++ _main.py
@@ -0,0 +1,27 @@
+import bluetooth
+import serial
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+port = 1
+server_sock.bind(("",port))
+server_sock.listen(1)
+
+print("In attesa di una connessione")
+client_sock,address = server_sock.accept()
+
+ser.write(b"BT")
+print("Connessione accetata da: ",address)
+try:
+    while True:
+        recv = client_sock.recv(1024)
+        print("input: ",recv)
+        #send = input()
+        #client_sock.send(str(send))
+        #ser.write(str(send).encode())
+except:
+    ser.write(b"BF")
+    print("Connessione scaduta")
+    client_sock.close()
+    server_sock.close()

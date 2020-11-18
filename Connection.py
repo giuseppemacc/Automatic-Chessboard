8164213b2a5d8c01c6fb6f5dfad83f17e683d700
giuseppe <giuseppemacc13@gmail.com>
2020-11-18 15:25:28 +0100

    update

---
+++ Connection.py
@@ -0,0 +1,77 @@
+import bluetooth
+import serial
+import asyncio
+import concurrent
+import time
+from os import system
+from shoot import shoot
+
+class Connection():
+    def __init__(self):
+        # variabili di connessione
+        self.ser_arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        self.server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        self.server_sock.bind(("",1))
+        self.server_sock.listen(1)
+
+    def init_connection(self):
+        print("In attesa di una connessione")
+        self.client_sock,self.address = self.server_sock.accept()
+
+        self.ser_arduino.write(b"BT")
+        print("Connessione accetata da: ",self.address)
+    
+    def close_connection(self):
+        self.ser_arduino.write(b"BF")
+        time.sleep(1)
+        self.ser_arduino.close()
+        self.client_sock.close()
+        self.server_sock.close()
+    
+    def do_on_serval(self,ser_val):
+        pass
+
+    async def ser_io(self):
+        while True:
+            executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+            loop = asyncio.get_event_loop()
+            ser_val = await loop.run_in_executor(executor, self.get_ser_val)
+            self.do_on_serval(ser_val)
+            #if ser_val == "SHOOT":
+            #    print("SHOOT")
+            #    shoot()
+
+            #elif ser_val == "SHUTDOWN":
+            #    print("Spegnimento...")
+            #    self.close_connection()
+            #    time.sleep(5)
+            #    quit() #system("shutdown now")
+
+    async def ble_io(self):
+        while True:
+            executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+            loop = asyncio.get_event_loop()
+            try:
+                recv = await loop.run_in_executor(executor, self.get_ble_val)
+                print("input: ",recv)
+            except:
+                self.ser_arduino.write(b"BF")
+                print("Connessione scaduta")
+                self.client_sock.close()
+                self.server_sock.close()
+        
+
+
+    # ===== Funzioni sync da rendere async ================
+    def get_ble_val(self):
+        return self.client_sock.recv(1024)
+
+    def get_ser_val(self):
+        return str(self.ser_arduino.readline())[2:-1]
+
+    def send_ble(self,value):
+        self.client_sock.send(value)
+    
+    def send_ser(self,value):
+        self.ser_arduino.write(value)
+    # ====================================================
--- Game.py
+++ Game.py
@@ -1,82 +1,11 @@
-import bluetooth
-import serial
-import asyncio
-import concurrent
-import time
-from os import system
-from shoot import shoot
-from Chessboard import Chessboard
+from Connection import Connection
 
-class Game_connection():
-    def __init__(self):
-        # variabili di connessione
-        self.ser_arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        self.server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-        self.server_sock.bind(("",1))
-        self.server_sock.listen(1)
-
-        # inizializza connessione
-        print("In attesa di una connessione")
-        self.client_sock,self.address = self.server_sock.accept()
-
-        self.ser_arduino.write(b"BT")
-        print("Connessione accetata da: ",self.address)
-
-    def get_ble_val(self):
-        return self.client_sock.recv(1024)
-
-    def get_ser_val(self):
-        return str(self.ser_arduino.readline())[2:-1]
-
-    def send_ble(self,value):
-        self.client_sock.send(value)
-    
-    def send_ser(self,value):
-        self.ser_arduino.write(value)
-
-
-    async def ser_io(self):
-        while True:
-            executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-            loop = asyncio.get_event_loop()
-            ser_val = await loop.run_in_executor(executor, self.get_ser_val)
-            if ser_val == "SHOOT":
-                print("SHOOT")
-                shoot()
-
-            elif ser_val == "SHUTDOWN":
-                self.close_connection()
-                self.shutdown
-
-    async def ble_io(self):
-        while True:
-            executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-            loop = asyncio.get_event_loop()
-            try:
-                recv = await loop.run_in_executor(executor, self.get_ble_val)
-                print("input: ",recv)
-            except:
-                self.ser_arduino.write(b"BF")
-                print("Connessione scaduta")
-                self.client_sock.close()
-                self.server_sock.close()
-                
-
-    def shutdown(self):
-        print("Spegnimento...")
-        time.sleep(5)
-        quit()
-        #system("shutdown now")
-
-    def close_connection(self):
-        self.ser_arduino.write(b"BF")
-        time.sleep(1/60)
-        self.ser_arduino.close()
-        self.client_sock.close()
-        self.server_sock.close()
-
-
-class Game(Game_connection):
+class Game(Connection):
     def __init__(self):
         super().__init__()
-
+        self.init_connection()
+    
+    #@override
+    def do_on_serval(self, ser_val):
+        if ser_val == "SHOOT":
+            print("SHOOT")

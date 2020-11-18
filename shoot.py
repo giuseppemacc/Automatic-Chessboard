0e72dceea165f9067b7707c5a70680eddb0e3d33
giuseppe <giuseppemacc13@gmail.com>
2020-11-18 14:33:34 +0100

    Update shoot.py

--- shoot.py
+++ shoot.py
@@ -1,9 +1,11 @@
 from picamera import PiCamera
 from time import sleep
 
+c = PiCamera()
+c.start_preview()
 def shoot():
-    c = PiCamera()
-    c.start_preview()
+    
+    
     sleep(0.5)
     c.capture("image/shoot.jpg")
-    c.stop_preview()
+    #c.stop_preview()
